48074abe8846206895704a88cbe7ef5d737e4dbe
Gera <gerardo.mdn95@gmail.com>
2024-06-10 18:27:47 +0900

    feat(clock): add clock game

---
+++ script.js
@@ -0,0 +1,18 @@
+// alert("Bienvenida");
+
+setInterval(() => {
+    var currentTime = new Date().toLocaleTimeString();
+    time.innerText = currentTime;
+}, 1000);
+
+function Result(x) {
+    var celebration = "url('image.celebration.png')";
+    var explotion = "url('image.explotion.png')";
+    if(x == 1) {
+        alert("Correcto");
+        document.body.style.backgroundImage = celebration;
+    } else {
+        alert("💥💥💥💥💥💥");
+        document.body.style.backgroundImage = explotion;
+    }
+}
